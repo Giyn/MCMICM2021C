7d9cde882d3d9742e0bc744e83df7282dd12190d
许继元 <490601115@qq.com>
2021-02-08 11:45:05 +0800

    feat: count species number

---
+++ utils/CountSpeciesNumber.py
@@ -0,0 +1,35 @@
+"""
+-------------------------------------
+# -*- coding: utf-8 -*-
+# @Time    : 2021/2/8 9:22:55
+# @File    : CountSpeciesNumber.py
+# @Software: PyCharm
+-------------------------------------
+"""
+
+import json
+
+with open('../data/negative_samples_categorize.txt', 'r') as file:
+    species_dict = json.loads(file.read().replace('\'', '\"'))
+
+with open('../data/negative_species.txt', 'r') as file:
+    species_list = list(map(lambda x: x.strip(), file.readlines()))
+
+
+print(species_dict)
+print(species_list)
+
+species_num_dict = {}
+
+# 初始化
+for name in species_list:
+    species_num_dict[name] = 0
+
+print(species_num_dict)
+
+# 统计
+for key, value in species_dict.items():
+    if value in species_list:
+        species_num_dict[value] += 1
+
+print(species_num_dict)
